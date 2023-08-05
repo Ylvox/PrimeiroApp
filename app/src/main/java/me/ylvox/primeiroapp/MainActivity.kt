d967e351ae13d37566437a2d35879f50bb9b2c41
Ylvox <87341143+Ylvox@users.noreply.github.com>
2023-08-05 00:53:49 -0300

    Initial commit

---
+++ app/src/main/java/me/ylvox/primeiroapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.ylvox.primeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun aoClicar(view: View) {
+        var txt = findViewById<TextView>(R.id.textView)
+        txt.text = "IHUULLLLLLL"
+    }
+}
